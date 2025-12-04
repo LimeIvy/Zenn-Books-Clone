f29ce3b36f36042134d19e163cdb4efed89cf9ab
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-12-04 17:50:01 +0900

    本一覧を取得する

--- backend/src/books.ts
+++ backend/src/books.ts
@@ -1,6 +1,15 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { connection } from "./db";
 
 export const books = new Hono()
-  .get('/', (c) => c.json('list books'))
-  .post('/', (c) => c.json('create a book', 201))
-  .get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
+  .get("/", async (c) => {
+    try {
+      const books = await connection.query("SELECT * FROM books");
+      return c.json(books[0]);
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+  .post("/", (c) => c.json("create a book", 201))
+  .get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
