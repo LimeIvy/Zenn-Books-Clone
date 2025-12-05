721c67aaeb5b92027323b0015e6f45391eb65d65
LimeIvy <raimu10311221@yahoo.co.jp>
2025-12-05 15:36:11 +0900

    本のCRUD操作のAPIを作成

--- backend/src/books.ts
+++ backend/src/books.ts
@@ -1,15 +1,86 @@
 import { Hono } from "hono";
 import { connection } from "./db";
+import { uuidv7 } from "uuidv7";
 
 export const books = new Hono()
+  /* 本一覧の取得 */
   .get("/", async (c) => {
     try {
-      const books = await connection.query("SELECT * FROM books");
+      const books = await connection.query(
+        "SELECT BIN_TO_UUID(CAST(id AS BINARY(16))) AS id, title, auther, description FROM books;"
+      );
       return c.json(books[0]);
     } catch (error) {
       console.error(error);
       return c.json({ message: "Database error" }, 500);
     }
   })
-  .post("/", (c) => c.json("create a book", 201))
-  .get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
+  /* 本の新規追加 */
+  .post("/", async (c) => {
+    const body = await c.req.json();
+    const title = body.title;
+    const auther = body.auther;
+    const description = body.description;
+    console.log(title, auther, description);
+    try {
+      const id = uuidv7();
+      await connection.query(
+        `INSERT INTO books (id, title, auther, description) VALUES (UUID_TO_BIN(?), ?, ?, ?);`,
+        [id, title, auther, description]
+      );
+      return c.json({ message: "Success" });
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+  /* 本の詳細情報の取得 */
+  .get("/:id", async (c) => {
+    const id = c.req.param("id");
+    console.log(id);
+
+    try {
+      const book = await connection.query(
+        `SELECT BIN_TO_UUID(CAST(id AS BINARY(16))) AS id, title, auther, description, created_at FROM books WHERE id = UUID_TO_BIN(?);`,
+        [id]
+      );
+
+      return c.json(book[0]);
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+
+  /* 本の詳細情報の更新 */
+  .put("/:id", async (c) => {
+    const id = c.req.param("id");
+    const body = await c.req.json();
+    const title = body.title;
+    const auther = body.auther;
+    const description = body.description;
+
+    try {
+      await connection.query(
+        `UPDATE books SET title = ?, auther = ?, description = ? WHERE id = UUID_TO_BIN(?);`,
+        [title, auther, description, id]
+      );
+
+      return c.json({ message : "Success" });
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+  /* 本の削除 */
+  .delete("/:id", async (c) => {
+    try {
+      await connection.query("DELETE FROM books WHERE id = UUID_TO_BIN(?);", [
+        c.req.param("id"),
+      ]);
+      return c.json({ message: "Success" });
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  });
--- backend/src/openapi.ts
+++ backend/src/openapi.ts
@@ -1,30 +1,97 @@
+import { request } from "http";
+
 export const openApiDoc = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'API Documentation',
-    version: '1.0.0',
-    description: 'API documentation for your service',
+    title: "Zenn Books Clone API Documentation",
+    version: "1.0.0",
+    description: "Zennの書籍機能のクローンアプリです。",
   },
   paths: {
-    '/books': {
+    "/books": {
       get: {
-        summary: 'books',
+        summary: "本一覧の取得",
         responses: {
-          '200': {
-            description: 'OK',
+          "200": {
+            description: "OK",
+          },
+        },
+      },
+      post: {
+        summary: "本の新規追加",
+        requestBody: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  title: {
+                    type: "string",
+                  },
+                  auther: {
+                    type: "string",
+                  },
+                  description: {
+                    type: "string",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          "200": {
+            description: "OK",
           },
         },
       },
     },
-    '/chapters': {
+    "/books/{id}": {
       get: {
-        summary: 'chapter',
+        summary: "本の詳細情報の取得・全チャプター取得",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            required: true,
+            schema: {
+              type: "string",
+            },
+          },
+        ],
         responses: {
-          '200': {
-            description: 'OK',
+          "200": {
+            description: "OK",
+          },
+        },
+      },
+      put: {
+        summary: "本の詳細情報の更新",
+        responses: {
+          "200": {
+            description: "OK",
+          },
+        },
+      },
+      delete: {
+        summary: "本の削除",
+        responses: {
+          "200": {
+            description: "OK",
+          },
+        },
+      },
+    },
+    "/chapters": {
+      get: {
+        summary: "chapter",
+        responses: {
+          "200": {
+            description: "OK",
           },
         },
       },
     },
   },
-}
+};
