8f6cc4a1e975c0361a8d4110ba04d1783f018113
LimeIvy <raimu10311221@yahoo.co.jp>
2025-12-06 16:43:23 +0900

    chapter側APIの作成

--- backend/src/chapters.ts
+++ backend/src/chapters.ts
@@ -1,5 +1,78 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { connection } from "./db";
+import { zValidator } from "@hono/zod-validator";
+
+import { querySchema } from "./schema";
+import { chaptersSchema } from "./schema";
 
 export const chapters = new Hono()
-  .get('/', (c) => c.json('list chapters'))
-  .get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
+  /* チャプターの新規作成 */
+  .post("/", zValidator("json", chaptersSchema), async (c) => {
+    const book_id = c.req.param("book_id");
+    const body = await c.req.valid("json");
+    const chapter_number = body.chapter_number;
+    const name = body.name;
+    const content = body.content;
+    try {
+      await connection.query(
+        `INSERT INTO chapters (book_id, chapter_number, name, content) VALUES ( UUID_TO_BIN(?), ?, ?, ?);`,
+        [book_id, chapter_number, name, content]
+      );
+      return c.json({ message: "Success" });
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+  /* チャプター内容の取得 */
+  .get("/:chapter_number", zValidator("param", querySchema), async (c) => {
+    const book_id = c.req.param("book_id");
+    const chapter_number = c.req.param("chapter_number");
+
+    try {
+      const chapters = await connection.query(
+        `SELECT name, content FROM chapters WHERE book_id = UUID_TO_BIN(?) AND chapter_number = ?;`,
+        [book_id, chapter_number]
+      );
+
+      return c.json(chapters[0]);
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+  /* チャプター内容の更新 */
+  .put("/:chapter_number", zValidator("json", chaptersSchema), async (c) => {
+    const book_id = c.req.param("book_id");
+    const chapter_number = c.req.param("chapter_number");
+    const body = await c.req.valid("json");
+    const name = body.name;
+    const content = body.content;
+
+    try {
+      await connection.query(
+        `UPDATE chapters SET chapter_number = ?, name = ?, content = ? WHERE book_id = UUID_TO_BIN(?) AND chapter_number = ?;`,
+        [chapter_number, name, content, book_id, chapter_number]
+      );
+
+      return c.json({ message: "Success" });
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
+  /* チャプターの削除 */
+  .delete("/:chapter_number", zValidator("param", querySchema), async (c) => {
+    const book_id = c.req.param("book_id");
+    const chapter_number = c.req.param("chapter_number");
+    try {
+      await connection.query(
+        "DELETE FROM chapters WHERE book_id = UUID_TO_BIN(?) AND chapter_number = ? ;",
+        [book_id, chapter_number]
+      );
+      return c.json({ message: "Success" });
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  });
