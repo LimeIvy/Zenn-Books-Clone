ccddb64c6bf59057f0d9831a0d3e0f239a444370
LimeIvy <raimu10311221@yahoo.co.jp>
2025-12-06 23:23:58 +0900

    新規作成ページの作成

---
+++ frontend/src/app/books/new/page.tsx
@@ -0,0 +1,11 @@
+import CreateBookForm from "@/features/books/components/CreateBookForm";
+
+export default function NewBook() {
+  return (
+    <div className="min-h-screen">
+      <main className="px-[5%] py-8 mt-10">
+       <CreateBookForm />
+      </main>
+    </div>
+  );
+}
