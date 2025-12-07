162cf38377c8a496fdad7431efdce24f15fc8a92
Lime_Ivy <raimu10311221@yahoo.co.jp>
2025-12-07 23:39:00 +0900

    チャプターの内容の保存機能を作成

--- frontend/src/features/books/components/MarkdownEditor.tsx
+++ frontend/src/features/books/components/MarkdownEditor.tsx
@@ -1,25 +1,51 @@
 "use client";
 
-import { useState } from 'react';
+import { useParams } from 'next/navigation';
 import { useAtomValue } from 'jotai';
+import { useChapter } from "@/features/books/hooks/useChapter";
 import { PreviewMarkdown } from "@/features/books/components/PreviewMarkdown";
 import { SimpleMdeEditor } from "@/features/books/components/SimpleMdeEditor";
 import { isPreviewAtom } from '../stores/Atom';
+import { Chapter } from '../types/chapter';
+import Spinner from '@/features/shared/components/ui/Spinner';
 
 export const MarkdownEditor = () => {
-  const [markdownValue, setMarkdownValue] = useState("");
+  const params = useParams();
+  const bookId = params.bookId as string;
+  const chapterNumber = params.chapterNumber as string;
+  const { chapter, isChapterLoading, isChapterError, mutateChapter } = useChapter(bookId, chapterNumber);
+  const isPreview = useAtomValue(isPreviewAtom);
 
-  const onChange = (value: string) => {
-    setMarkdownValue(value);
-  };
+  if (isChapterLoading) {
+    return (
+      <div className="mt-50 flex items-center justify-center">
+        <Spinner size={80} />
+      </div>
+    );
+  }
+
+  if (isChapterError) {
+    return (
+      <div className="flex items-center justify-center z-10">
+        <p>本の取得に失敗しました。</p>
+      </div>
+    );
+  }
 
   return (
     <div className="w-full h-full flex flex-col">
-      <input name="name" type="text" className="text-2xl text-white font-bold mb-8" placeholder="Title" />
-      {useAtomValue(isPreviewAtom) ? (
-        <PreviewMarkdown markdownValue={markdownValue} />
+      <input
+        name="name"
+        type="text"
+        value={chapter?.name || ""}
+        onChange={(e) => mutateChapter({ ...chapter, name: e.target.value } as Chapter, false)}
+        className="text-3xl text-white font-bold mb-8 focus:outline-none"
+        placeholder="Title"
+      />
+      {isPreview ? (
+        <PreviewMarkdown markdownValue={chapter?.content || ""} />
       ) : (
-        <SimpleMdeEditor markdownValue={markdownValue} onChange={onChange} />
+        <SimpleMdeEditor markdownValue={chapter?.content || ""} onChange={(value) => mutateChapter({ ...chapter, content: value } as Chapter, false)} />
       )}
     </div>
   );
--- frontend/src/features/books/components/ToolSideBar.tsx
+++ frontend/src/features/books/components/ToolSideBar.tsx
@@ -1,20 +1,42 @@
 "use client";
 import { useAtomValue, useSetAtom } from 'jotai';
+import { useParams } from 'next/navigation';
+import { useChapter } from '@/features/books/hooks/useChapter';
+import { client } from '@/features/shared/utils/client';
 import { isPreviewAtom } from '../stores/Atom';
 import { FaPen } from "react-icons/fa";
 import { IoPlayOutline } from "react-icons/io5";
 import Button from '@/features/shared/components/ui/Button';
 
 export const ToolSideBar = () => {
+  const params = useParams();
+  const bookId = params.bookId as string;
+  const chapterNumber = params.chapterNumber as string;
+  const { chapter, mutateChapter } = useChapter(bookId, chapterNumber);
   const isPreview = useAtomValue(isPreviewAtom);
   const setIsPreview = useSetAtom(isPreviewAtom);
 
+  const handleSave = async () => {
+    if (!chapter || !chapterNumber || !chapter.name) {
+      alert("チャプターの情報が不足しています");
+      return;
+    }
+    try {
+      await client.books[":book_id"].chapters[":chapter_number"].$put({ 
+        param: { book_id: bookId, chapter_number: chapterNumber }, 
+        json: { chapter_number: parseInt(chapterNumber), name: chapter.name, content: chapter.content || "" } 
+      });
+      alert("チャプターの保存に成功しました");
+    } catch (error) {
+      console.error("チャプターの保存に失敗しました:", error);
+    } finally {
+      mutateChapter(chapter);
+    }
+  };
   return (
     <div className="w-full h-full flex flex-col items-center gap-5">
       <div>
-        <Button
-          onClick={() => setIsPreview(false)}
-        >
+        <Button onClick={handleSave}>
           <p>保存</p>
         </Button>
       </div>
