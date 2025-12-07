d38bf6cb6ba55cb8beccd9ab7eccad0b21a50c1f
LimeIvy <raimu10311221@yahoo.co.jp>
2025-12-07 14:30:25 +0900

    チャプター一覧を取得するAPiの作成

--- backend/src/chapters.ts
+++ backend/src/chapters.ts
@@ -6,6 +6,22 @@ import { querySchema } from "./schema";
 import { chaptersSchema } from "./schema";
 
 export const chapters = new Hono()
+  /* チャプター一覧の取得 */
+  .get("/", async (c) => {
+    const book_id = c.req.param("book_id");
+
+    try {
+      const chapters = await connection.query(
+        `SELECT BIN_TO_UUID(CAST(id AS BINARY(16))) AS id, chapter_number, name FROM chapters WHERE book_id = UUID_TO_BIN(?);`,
+        [book_id]
+      );
+
+      return c.json(chapters[0]);
+    } catch (error) {
+      console.error(error);
+      return c.json({ message: "Database error" }, 500);
+    }
+  })
   /* チャプターの新規作成 */
   .post("/", zValidator("json", chaptersSchema), async (c) => {
     const book_id = c.req.param("book_id");
